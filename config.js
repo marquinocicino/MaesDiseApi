102b9812d7955cba90ecf308b2533ef20920c0b4
Marco Capon <33531757+marquinocicino@users.noreply.github.com>
2023-10-15 10:36:49 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    PORT: 3000, 
+    DB_URL: 'mongodb+srv://marco1989delrio:O2jy9UeoM6Mdo5Qk@cluster0.kajjcpa.mongodb.net/?retryWrites=true&w=majority'            
+}
+
+module.exports = config
